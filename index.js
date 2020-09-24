92d47ec74aaf4aa4896c95a187a542a10e272136
“Joshua <punshon.jm@live.com.au>
2020-09-24 22:54:19 +1000

    add youtube time parsing

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-// Primarily for local development
+// For local development
 require('dotenv').config();
 
 const axios = require('axios').default;
 const { Readability } = require('@mozilla/readability');
 const { JSDOM } = require('jsdom');
+const youtube = require('ytdl-core');
 
 const {
   RAINDROP_TOKEN: token,
   RAINDROP_COLLECTION: collectionId = '-1',
   CONFIG_WPM = '250',
   CONFIG_TAG_PREFIX: tagPrefix = 'time-',
+  NODE_ENV: environment,
 } = process.env;
 
 const wpm = parseInt(CONFIG_WPM, 10);
@@ -22,46 +24,86 @@ const api = axios.create({
   headers: { Authorization: `Bearer ${token}` },
 });
 
-exports.processUnsorted = async (req, res) => {
-  console.log('Processing your droplets...');
+const hasTimeTag = (tags) => tags.find((tag) => tag.includes(tagPrefix));
 
-  let updated = 0;
+const updateTags = async (id, tags, length) => {
+  const tag = `${tagPrefix}${Math.round(length)}mins`;
+
+  return api.put(`/raindrop/${id}`, {
+    tags: [...tags, tag],
+  });
+};
+
+const calculateYouTube = async ({ _id, tags, link }) => {
+  // Get time of video if its from YouTube domain
+  const { videoDetails } = await youtube.getBasicInfo(link);
+  const minutes = parseInt(videoDetails.lengthSeconds, 10) / 60;
+  await updateTags(_id, tags, minutes);
+};
+
+const getAllDrops = async () => {
   const drops = [];
 
   for (let x = 0; x < 50; x++) {
     const { data: { items } } = await api.get(`/raindrops/${collectionId}?perpage=50&page=${x}`);
     drops.push(...items);
+
     if (items.length === 0) {
       // end the loop early for pages with less than 50 items
       x = 50;
     }
   }
 
-  console.log(`Found ${drops} droplets to process...`);
+  return drops;
+};
+
+const calculateContent = async ({ _id, tags, link }) => {
+  // Download and parse the document
+  const { data: source } = await axios.get(link);
+  const doc = new JSDOM(source, { url: link });
+  const { textContent } = new Readability(doc.window.document, {}).parse();
+
+  // estimate the number of minutes to read content
+  const words = textContent.match(/(\w+)/g).length;
+  const minutes = words / wpm;
+
+  // Add the time tag to the bookmark
+  await updateTags(_id, tags, minutes);
+};
+
+exports.processUnsorted = async (req, res) => {
+  console.log('Processing your droplets...');
+
+  let updated = 0;
+  const drops = await getAllDrops();
+
+  console.log(`Found ${drops.length} droplets to process...`);
 
   await Promise.all(drops.map(async (item) => {
     try {
-      const { tags, link, _id } = item;
-
-      const hasTime = tags.find((tag) => tag.includes(tagPrefix));
-      if (hasTime) {
+      // Only process bookmarks without a time tag yet
+      if (hasTimeTag(item.tags)) {
         return;
       }
 
-      const { data: source } = await axios.get(link);
-      const doc = new JSDOM(source, { url: link });
-      const { textContent } = new Readability(doc.window.document, {}).parse();
-      const words = textContent.match(/(\w+)/g).length;
-      const minutes = Math.round(words / wpm);
+      // Process youtube.com videos
+      if (item.domain.includes('youtube.com')) {
+        await calculateYouTube(item);
+        updated += 1;
+        return;
+      }
 
-      const tag = `${tagPrefix}${minutes}mins`;
-      await api.put(`/raindrop/${_id}`, {
-        tags: [...tags, tag],
-      });
-
+      // Try process all other content
+      await calculateContent(item);
       updated += 1;
     } catch (error) {
-      console.error(error);
+      if (environment === 'development') {
+        console.error(error);
+      }
+
+      // Add a 0 minute tag so we don't repeatedly
+      // try process the same droplet
+      await updateTags(item._id, item.tags, 0);
     }
   }));
 
